jasonrohrer/OneLife

add button to protect sensitive fields from accidental clicking

gameSource/ExistingAccountPage.h
@@ -92,6 +92,7 @@ class ExistingAccountPage : public GamePage, public ActionListener {
         Background mGameLogo;
         
         TextButton mSeedButton;
+        TextButton mUnlockButton;
         
         TextButton mLoginButton;
         TextButton mFriendsButton;

connorhsm/OneLife-1

gameSource/ExistingAccountPage.h
@@ -91,6 +91,7 @@ class ExistingAccountPage : public GamePage, public ActionListener {
         Background mBackground;
         Background mGameLogo;
         
+        TextButton mSeedButton;
         TextButton mLoginButton;
         TextButton mFriendsButton;
         TextButton mGenesButton;